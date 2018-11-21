9415651e88b2bca8e7d7eb27e695fbac5620815e
VSemenyshyn <viktoriia.palenka@gmail.com>
2018-11-21 16:08:39 +0200

    Kikacookies website

---
+++ kikacookies website/resources/js/script.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    
+    
+    
+    /* Sticky Nav  */
+     $('.js--section-story').waypoint(function(direction) {
+       if (direction ==  "down") {
+           $('nav').addClass('sticky');
+       } else { 
+           $('nav').removeClass('sticky');
+       }
+       }, { 
+           offset: '60px;'
+    });
+    
+    
+    /* Scroll Btn  */
+    $('.js--scroll-to-order').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-order').offset().top}, 1000);
+    });
+    
+    
+     /* Smooth Scroll */
+    
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+    
+    
+    /* Map  */
+    var map = new GMaps({
+  div: '.map',
+  lat: 50.7521773,
+  lng: 25.3588
+});
+    map.addMarker({
+  lat: 50.7521773,
+  lng: 25.35,
+  title: 'Lutsk',
+  infoWindow: {
+  content: '<p>Our Lutsk Office</p>'
+}
+});
+    
+   
+
+
+});
